cca5a79d2ab212c40a94f940912b9d30dd400eeb
daddy <mardombet@gmail.com>
2018-11-19 22:31:45 +0100

    "final"

--- test.py
+++ test.py
@@ -11,12 +11,17 @@ ml.reset()
 mr.reset()
 mm.reset()
 
+ultrapos = 0
+
+forwa = 0
+lef = 0
+righ = 0
 
 #          0 = forw
 #3 = left            1 = right
 #          2 = back
 rotrob = 0
-
+posrob = [3,4]
 
 #0 = nevi
 #1 = block
@@ -24,18 +29,18 @@ rotrob = 0
 #3 = nic projeto
 #4 = robot
 
-pole = [[0,1,2,3,4,5,6,7,8],
-        [0,1,2,3,4,5,6,7,8],
-        [0,1,2,3,4,5,6,7,8],
-        [0,1,2,3,4,5,6,7,8],
-        [0,1,2,3,4,5,6,7,8],
-        [0,1,2,3,4,5,6,7,8]]
+pole = [[0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,1,3,1,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0]]
 
 
 #movement
 def forw():
-        ml.run_to_rel_pos(speed_sp = 500, position_sp = 360, stop_action = "hold")
-        mr.run_to_rel_pos(speed_sp = 500, position_sp = 360, stop_action = "hold")
+        ml.run_to_rel_pos(speed_sp = 500, position_sp = 5, stop_action = "hold")
+        mr.run_to_rel_pos(speed_sp = 500, position_sp = 5, stop_action = "hold")
 
 def left():
         ml.run_to_rel_pos(speed_sp = 500, position_sp = -360, stop_action = "hold")
@@ -51,17 +56,47 @@ def back():
 
 
 #sensor
-def ultraread(side):
+def ultraread(side,ultrapos):
         if side == 0:
-                mm.run_to_rel_pos(speed_sp = 250, position_sp = 90)
+                mm.run_to_rel_pos(speed_sp = 200, position_sp = (ultrapos)*-1)
+                mm.wait_until_not_moving()
+                ultrapos = (ultrapos)*-1
                 return ultra.distance_centimeters()
         elif side == 1:
-                mm.run_to_rel_pos(speed_sp = 250, position_sp = 90)
+                mm.run_to_rel_pos(speed_sp = 200, position_sp = (90+ultrapos)*-1)
+                mm.wait_until_not_moving()
+                ultrapos = (90+ultrapos)*-1
                 return ultra.distance_centimeters()
         elif side == 3:
-                mm.run_to_rel_pos(speed_sp = 250, position_sp = 90)
+                mm.run_to_rel_pos(speed_sp = 200, position_sp = 90+(ultrapos*-1))
+                mm.wait_until_not_moving()
+                ultrapos = 90+(ultrapos*-1)
                 return ultra.distance_centimeters()
 
 
-
+def mapupdate(position, direction, forw, right, left):
+        if direction == 0:
+                for x in range(forw):
+                        pole[position[0]-x-1][position[1]] = 2
+                pole[position[0]-forw-1][position[1]] = 1
+                for x in range(left):
+                        pole[position[0]][position[1]-x-1] = 2
+                pole[position[0]][position[1]-left-1] = 1
         
+
+
+
+
+while True:
+        forwa = ultraread(0,ultrapos)
+        righ = ultraread(1,ultrapos)
+        lef = ultraread(3,ultrapos)
+
+        if forw > right and forw > left:
+                while ultraread(0,ultrapos) < 8:
+                        forw()
+        elif right > forw and right > left:
+                right()
+        elif left > forw and left > right:
+                left()
+
